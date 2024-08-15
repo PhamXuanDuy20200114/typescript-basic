cf93aa188f73ac05ac1ff126da410e7731409648
PhamXuanDuy_20200114 <phamduy06112002@gmail.com>
2024-08-15 22:18:20 +0700

    lesson 7 ts tự gán kiểu dữ liệu, lesson 8 kiểu dữ liệu number

---
+++ project/lesson8/lesson8.js
@@ -0,0 +1,2 @@
+let count = 10; //int long  => number
+let score = 1.11; //float double => number
---
+++ project/lesson8/lesson8.ts
@@ -0,0 +1,2 @@
+let count1: number; //Định nghĩa kiểu dữ liệu số mà không gán giá trị
+let score1:number = 1.11; //Định nghĩa kiểu dữ liệu số mà có gán giá trị
